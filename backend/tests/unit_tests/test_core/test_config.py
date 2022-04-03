5f058b282d9dea419ed1b499c907d34611767ed6
Eternal-Engine <ninovationlab@gmail.com>
2022-04-03 16:10:40 +0200

    test(Settings): fastapi_kwargs() should rewrite FastAPI attributes
    The function fastapi_kwargs should return Settings new attributes
    that initialize/rewrite the default value of FastAPI class attributes.

--- backend/tests/unit_tests/test_core/test_config.py
+++ backend/tests/unit_tests/test_core/test_config.py
@@ -1,23 +1,57 @@
+import unittest
+
 from pydantic import BaseSettings
 
 from app.core.config import Settings, get_settings
 
 
-def test_settings_construction():
+class TestSettings(unittest.TestCase):
+    def setUp(self):
+        self.pydantic_base_settings = BaseSettings()
+        self.settings = Settings()
+        self.settings_from_get_settings = get_settings()
 
-    pydantic_base_settings = BaseSettings
-    settings = Settings
+    def test_create_settings(self):
 
-    assert issubclass(settings, pydantic_base_settings)
-    assert settings().__str__() == "Application Settings"
-    assert settings().ENV == "dev"
-    assert settings().TESTING == 1
-    assert settings().database_url == "sqlite:///iW_dev.sqlite3"
+        settings = self.settings
+        pydantic_base_settings = self.pydantic_base_settings
+        self.assertTrue(
+            issubclass(
+                type(settings),
+                type(pydantic_base_settings),
+            )
+        )
+        self.assertEqual("Application Settings", settings.__str__())
+        self.assertEqual("dev", settings.ENV)
+        self.assertEqual("sqlite:///iW_dev.sqlite3", settings.database_url)
 
+        # Settings attributes for FastAPI setup
+        self.assertEqual("iWitness - Backend Development Application", settings.title),
+        self.assertEqual(
+            "A backend project with FastAPI for iWitness web application.",
+            settings.description,
+        ),
+        self.assertEqual("0.0.0", settings.version),
+        self.assertEqual(True, settings.debug),
+        self.assertEqual("/docs", settings.docs_url),
+        self.assertEqual("", settings.openapi_prefix),
+        self.assertEqual("openapi.json", settings.openapi_url),
+        self.assertEqual("/redoc", settings.redoc_url)
 
-def test_get_settings_function_to_get_application_settings():
+    def test_retrieve_settings_from_get_settings(self):
 
-    settings = Settings
-    settings_from_get_settings = get_settings()
-
-    assert isinstance(settings_from_get_settings, settings)
+        settings = self.settings
+        settings_from_get_settings = self.settings_from_get_settings
+        expected = {
+            "title": "iWitness - Backend Development Application",
+            "description": "A backend project with FastAPI for iWitness web application.",
+            "version": "0.0.0",
+            "debug": True,
+            "docs_url": "/docs",
+            "openapi_prefix": "",
+            "openapi_url": "openapi.json",
+            "redoc_url": "/redoc",
+        }
+        self.assertIsInstance(settings_from_get_settings, type(settings))
+        self.assertEqual(settings_from_get_settings, settings)
+        self.assertEqual(expected, settings_from_get_settings.fastapi_kwargs)
