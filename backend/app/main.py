1975712fa5f262e37b0597bff702fe9305005cc3
Eternal-Engine <ninovationlab@gmail.com>
2022-04-04 20:40:43 +0200

    feat(db): Run the async db driver with startup function

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,16 +3,22 @@ from fastapi import FastAPI
 from app.core.config import get_settings
 from app.db.database import Base, engine
 
-Base.metadata.create_all(bind=engine)
-
 settings = get_settings()
 app = FastAPI(**settings.fastapi_kwargs)
+
+
+@app.on_event("startup")
+async def startup():
+    # create db tables
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 
 @app.get("/app_settings", tags=["App Settings"])
 async def get_app_settings():
 
     return {
-        "environment": settings.ENV,
+        "environment": settings.app_env,
         "database_url": settings.database_url,
     }
