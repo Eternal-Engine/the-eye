dcc97a583a13516416c97501196e008434fc78b1
Eternal-Engine <ninovationlab@gmail.com>
2022-04-04 16:42:46 +0200

    ref(settings): Create the base classes for the 3 app environments
    This is the first step to achieve SRP.
    
    Both classes `EnvTypes` and `AppBaseSettings` have only a single responsibility:
        - `EnvTypess` is the source of the 3 app environments.
        - `AppBaseSettings` is the container for 1 specific app environment
        with the default value of the production environment.

--- backend/app/core/settings/app_base_settings.py
+++ backend/app/core/settings/app_base_settings.py
@@ -1,8 +1,27 @@
-class EnvTypes:
+from enum import Enum
 
-    pass
+from pydantic import BaseSettings
 
 
-class AppBaseSettings:
+class EnvTypes(Enum):
 
-    pass
+    PROD: str = "prod"
+    DEV: str = "dev"
+    TEST: str = "test"
+
+    def describe(self):
+        """
+        A function to inspect all EnvTypes attributes.
+        """
+        return f"{self.name}: {self.value}"
+
+    def __str__(self):
+        return f"{self.value.capitalize()} environment type"
+
+
+class AppBaseSettings(BaseSettings):
+
+    app_env: EnvTypes = EnvTypes.PROD
+
+    def __str__(self):
+        return "Base Settings for App Settings"
