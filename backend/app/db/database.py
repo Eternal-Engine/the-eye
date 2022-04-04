f7f7f7f2b3f168b5be586fd38d350cb427d4f5cd
Eternal-Engine <ninovationlab@gmail.com>
2022-04-04 20:36:11 +0200

    feat(db): Update db driver
    The database driver is now setup using AsyncPG and AIOSQLite. This will
    enable us to run the transaction asynchronously from the database.

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,26 +1,10 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from app.core.config import get_settings
 
-# Get the database url based on the environment settings
 settings = get_settings()
-
-# Connect database with SQLAlchemy
 DATABASE_URL = settings.database_url
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-# As the base class for db objects in models.py
+engine = create_async_engine(DATABASE_URL, future=True, echo=True)
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
-
-
-# Initialize db session
-def get_db() -> Session:
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
