382c49ec3d9810938a16d5fa77602061f3f6ef2f
Eternal-Engine <ninovationlab@gmail.com>
2022-04-04 16:34:13 +0200

    test(settings): The construction of 2 new classes to set app environment
    - `EnvTypes` should inherit from Python built-in class `Enum` and provide 3 attributes:
        - `PROD` for production environment
        - `DEV` for development environment
        - `TEST` for test environment
    
    - `EnvTypes` should have a function called `describe` that inspect the
    name of the attribute and its value.
    
    - `AppBaseSettings` should inherit from Pydantic built-in class `BaseSettings`
    and contains 1 attribute `app_env`.

---
+++ backend/app/core/settings/app_base_settings.py
@@ -0,0 +1,8 @@
+class EnvTypes:
+
+    pass
+
+
+class AppBaseSettings:
+
+    pass
---
+++ backend/tests/unit_tests/test_core/test_settings/test_app_base_settings.py
@@ -0,0 +1,54 @@
+from enum import Enum
+
+from app.core.settings.app_base_settings import AppBaseSettings, EnvTypes
+
+
+def test_create_env_types():
+
+    env_types = EnvTypes
+    # Test inheritance for EnvTypes from Enum built-in Python class
+    assert issubclass(type(Enum), type(env_types))
+    assert env_types.PROD.value == "prod"
+    assert env_types.DEV.value == "dev"
+    assert env_types.TEST.value == "test"
+    assert str(env_types.PROD) == "Prod environment type"
+    assert str(env_types.DEV) == "Dev environment type"
+    assert str(env_types.TEST) == "Test environment type"
+
+
+def test_get_env_type_name_and_value_by_describe_function():
+
+    env_types = EnvTypes
+
+    assert env_types.PROD.describe() == "PROD: prod"
+    assert env_types.DEV.describe() == "DEV: dev"
+    assert env_types.TEST.describe() == "TEST: test"
+
+
+# Test for AppBaseSettings
+def test_create_app_base_settings_with_prod_env_as_default_app_env():
+
+    env_types = EnvTypes
+    app_base_default_settings = AppBaseSettings()
+
+    assert str(app_base_default_settings) == "Base Settings for App Settings"
+    assert isinstance(app_base_default_settings.app_env, env_types)
+    assert app_base_default_settings.app_env.describe() == "PROD: prod"
+
+
+def test_create_app_base_settings_for_dev_env():
+
+    app_base_dev_settings = AppBaseSettings
+    app_base_dev_settings.app_env = EnvTypes.DEV
+
+    assert issubclass(EnvTypes, type(app_base_dev_settings().app_env))
+    assert app_base_dev_settings.app_env.describe() == "DEV: dev"
+
+
+def test_create_app_base_settings_for_test_env():
+
+    app_base_test_settings = AppBaseSettings
+    app_base_test_settings.app_env = EnvTypes.TEST
+
+    assert issubclass(EnvTypes, type(app_base_test_settings().app_env))
+    assert app_base_test_settings.app_env.describe() == "TEST: test"
