0ab5d2dd188c7787dd3c58c58d4614d9be799350
Eternal-Engine <ninovationlab@gmail.com>
2022-04-04 20:32:02 +0200

    test(main): Update unit-test for the app_settings route

--- backend/tests/unit_tests/test_app/test_main.py
+++ backend/tests/unit_tests/test_app/test_main.py
@@ -3,7 +3,7 @@ import pytest
 from fastapi import FastAPI
 from httpx import AsyncClient
 
-from app.main import app
+from app.main import app, settings
 
 
 def test_initalize_fastapi_app_in_app():
@@ -16,9 +16,9 @@ def test_initalize_fastapi_app_in_app():
 
 # Async test is not yet provided by unittest, hence using pytest
 @pytest.mark.anyio
-async def test_app_settings_route_response_200():
+async def test_async_app_settings_route_response_200():
 
-    expected = {"environment": "dev", "database_url": "sqlite:///iW_dev.sqlite3"}
+    expected = {"environment": "prod", "database_url": settings.database_url}
 
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/app_settings")
