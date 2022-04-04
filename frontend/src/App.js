dcfef794b48dd63ac4977af9e3528a7dda2b63ce
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2022-04-05 01:11:05 +0200

    feat/frontend setup with reactjs

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>iWitness is coming...</h1>
+      <button>Signup</button>
+    </div>
+  );
+}
+
+export default App;
