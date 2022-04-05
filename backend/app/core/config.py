33f246efb3c4d5d5086e0b296af29d74092f6f2c
Eternal-Engine <ninovationlab@gmail.com>
2022-04-05 20:45:57 +0200

    feat(settings): Create a dict container for all types of env
    The dictionary `environments` stores all 3 `EnvTypes` as `key` and all child
    classes from `AppSettings`; `AppProdSettings`, `AppDevSettings`, `AppTestSettings`,
    as the `value`.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,8 +1,17 @@
 from functools import lru_cache
+from typing import Dict, Type
 
+from app.core.settings.app_base_settings import EnvTypes
+from app.core.settings.app_dev_settings import AppDevSettings
+from app.core.settings.app_prod_settings import AppProdSettings
 from app.core.settings.app_settings import AppSettings
+from app.core.settings.app_test_settings import AppTestSettings
 
-app_environments: dict
+environments: Dict[EnvTypes, Type[AppSettings]] = {
+    EnvTypes.PROD: AppProdSettings,
+    EnvTypes.DEV: AppDevSettings,
+    EnvTypes.TEST: AppTestSettings,
+}
 
 
 @lru_cache()
--- backend/tests/unit_tests/test_core/test_config.py
+++ backend/tests/unit_tests/test_core/test_config.py
@@ -1,5 +1,5 @@
 # fmt: off
-from app.core.config import app_environments, get_settings
+from app.core.config import environments, get_settings
 from app.core.settings.app_base_settings import EnvTypes
 from app.core.settings.app_dev_settings import AppDevSettings
 from app.core.settings.app_prod_settings import AppProdSettings
@@ -10,17 +10,17 @@ from app.core.settings.app_test_settings import AppTestSettings
 def test_call_settings_from_function_get_settings():
 
     settings = get_settings()
-    app_settings = AppSettings
+    app_settings = AppSettings()
 
     assert settings == app_settings
 
 
 def test_create_app_environments():
 
-    app_env = app_environments
+    app_env = environments
     expected = {
-        EnvTypes.DEV: AppProdSettings,
-        EnvTypes.PROD: AppDevSettings,
+        EnvTypes.PROD: AppProdSettings,
+        EnvTypes.DEV: AppDevSettings,
         EnvTypes.TEST: AppTestSettings,
     }
 
