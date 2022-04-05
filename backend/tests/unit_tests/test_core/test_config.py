f1c5dc67742c6d64488406527c4b54cbe247736d
Eternal-Engine <ninovationlab@gmail.com>
2022-04-05 20:40:04 +0200

    test(settings): Create a container for all app env types
    This unit test ensures that all child classes from `AppSettings` are assigned
    to its respective type of `EnvTypes`:
    
        - `AppProdSettings` with `EnvTypes.PROD`

--- backend/tests/unit_tests/test_core/test_config.py
+++ backend/tests/unit_tests/test_core/test_config.py
@@ -1,6 +1,10 @@
 # fmt: off
 from app.core.config import app_environments, get_settings
+from app.core.settings.app_base_settings import EnvTypes
+from app.core.settings.app_dev_settings import AppDevSettings
+from app.core.settings.app_prod_settings import AppProdSettings
 from app.core.settings.app_settings import AppSettings
+from app.core.settings.app_test_settings import AppTestSettings
 
 
 def test_call_settings_from_function_get_settings():
@@ -8,11 +12,17 @@ def test_call_settings_from_function_get_settings():
     settings = get_settings()
     app_settings = AppSettings
 
-    assert isinstance(settings, app_settings)
+    assert settings == app_settings
 
 
 def test_create_app_environments():
 
     app_env = app_environments
+    expected = {
+        EnvTypes.DEV: AppProdSettings,
+        EnvTypes.PROD: AppDevSettings,
+        EnvTypes.TEST: AppTestSettings,
+    }
 
     assert isinstance(app_env, dict)
+    assert app_env == expected
