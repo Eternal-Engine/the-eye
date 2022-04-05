3bb93c5f888563d8b10f2521f9b4e2bfc0f09241
Eternal-Engine <ninovationlab@gmail.com>
2022-04-05 19:36:14 +0200

    test(settings): Create container for all 3 environment types

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,6 +2,8 @@ from functools import lru_cache
 
 from app.core.settings.app_settings import AppSettings
 
+app_environments: dict
+
 
 @lru_cache()
 def get_settings():
--- backend/tests/unit_tests/test_core/test_config.py
+++ backend/tests/unit_tests/test_core/test_config.py
@@ -1,5 +1,5 @@
 # fmt: off
-from app.core.config import get_settings
+from app.core.config import app_environments, get_settings
 from app.core.settings.app_settings import AppSettings
 
 
@@ -9,3 +9,10 @@ def test_call_settings_from_function_get_settings():
     app_settings = AppSettings
 
     assert isinstance(settings, app_settings)
+
+
+def test_create_app_environments():
+
+    app_env = app_environments
+
+    assert isinstance(app_env, dict)
