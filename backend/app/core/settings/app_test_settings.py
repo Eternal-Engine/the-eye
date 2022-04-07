8d4e57b86088b9209a7d2bdef8b9d83e1ec4c378
Eternal-Engine <ninovationlab@gmail.com>
2022-04-07 12:33:56 +0200

    ref: Remove class attr database_url
    Since `AppSettings` is changed into using `os.getenv()` for all secrets,
    the class attribute `database_url` in `AppTestSettings` is just a repetition
    becuase `AppTestSettings` inherits from `AppSettings`, thus is removed.

--- backend/app/core/settings/app_test_settings.py
+++ backend/app/core/settings/app_test_settings.py
@@ -1,12 +1,9 @@
-import os
-
-from app.core.settings.app_settings import AppSettings
+from backend.app.core.settings.app_settings import AppSettings
 
 
 class AppTestSettings(AppSettings):
 
     title: str = "iWitness - Backend Test Environment Settings"
-    database_url: str = os.getenv("DATABASE_URL")  # type: ignore
 
     class Config(AppSettings.Config):
-        env_file = "env/test_env/test.env"
+        env_file = "env/.env.test"
