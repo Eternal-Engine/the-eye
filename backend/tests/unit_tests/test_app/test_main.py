f924c6f1ca61953dab63691e0205097de07e1659
Eternal-Engine <ninovationlab@gmail.com>
2022-04-12 12:47:44 +0200

    test: Update test for endpoint  with

--- backend/tests/unit_tests/test_app/test_main.py
+++ backend/tests/unit_tests/test_app/test_main.py
@@ -1,7 +1,4 @@
-# fmt: off
-import pytest
 from fastapi import FastAPI
-from httpx import AsyncClient
 
 from app.main import app
 
@@ -14,14 +11,11 @@ def test_initalize_fastapi_app_in_app():
     assert isinstance(my_app, type(fastapi_app))
 
 
-# Async test is not yet provided by unittest, hence using pytest
-@pytest.mark.anyio
-async def test_async_app_settings_route_response_200():
+async def test_async_app_settings_route_response_200(async_client):
 
     expected = {"environment": "prod"}
 
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/app_settings")
+    response = async_client.get("/app_settings/")
 
     assert response.status_code == 200
     assert response.json() == expected
