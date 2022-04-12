a70454d3e68682395cbfb1fe242e5ca821b32ca8
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2022-04-12 10:49:25 +0200

    feat: added router-dom, refactored header component, added pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,26 @@
 //App component serves as container to our application. exported as child to ./index.js
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from "./components/Header"  
+import Signup from './pages/Signup'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Homepage from './pages/Homepage'
+import ForgotPassword from './pages/ForgotPassword'
 
-import Header from "./components/Header"  //import header component from components
-import headlines from './data/news'  //model data
 
 function App(){
  
     return (
-        <>   
-        <Header />
-        <div className="container">
-            
-        </div>
+        <>  
+          <Router>
+            <Header />  
+            <Routes>
+                <Route path='/' element={<Homepage />} />
+                <Route path='/sign-up' element={<Signup />} />
+                <Route path='/log-in' element={<Login />} />
+                <Route path='/forgot-password' element={<ForgotPassword />} />
+            </Routes>
+          </Router>
         </>
         
     )
---
+++ frontend/src/pages/Read.jsx
@@ -0,0 +1,9 @@
+function Read(){
+    return(
+        <div>
+            <h1>Read News page</h1>
+        </div>
+    )
+}
+
+export default Read
