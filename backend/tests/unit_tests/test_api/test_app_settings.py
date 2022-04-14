37985c9d126e33ca1c8134c3d639d20daee059b2
Eternal-Engine <ninovationlab@gmail.com>
2022-04-15 01:02:59 +0200

    Add await in all async_client reponse for testing user endpoints

--- backend/tests/unit_tests/test_api/test_app_settings.py
+++ backend/tests/unit_tests/test_api/test_app_settings.py
@@ -2,7 +2,7 @@ async def test_async_retrieve_app_settings_route(async_client):
 
     expected = {"environment": "production"}
 
-    response = async_client.get("/api/app_settings")
+    response = await async_client.get("/api/app_settings")
 
     assert response.status_code == 200
     assert response.json() == expected
