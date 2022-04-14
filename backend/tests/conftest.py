709ee081a4e288d79436093c93e336ff4b9a2c01
Eternal-Engine <ninovationlab@gmail.com>
2022-04-14 19:56:41 +0200

    ref(test): Move test_metadata and test_engine into create_and_drop_test_db() function

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,6 +1,8 @@
 # fmt: off
 # type: ignore
-import databases
+
+from typing import Generator
+
 import pytest
 import sqlalchemy
 from fastapi import FastAPI
@@ -12,40 +14,45 @@ from app.core.settings.app_base_settings import EnvTypes
 
 # Set up the `app_env` to use the TEST environment settings
 settings = get_settings(app_env=EnvTypes.TEST)
-TEST_DATABASE_URL = settings.database_url
-
-# Databases query builder
-test_database = databases.Database(TEST_DATABASE_URL, force_rollback=True)
-
-metadata = sqlalchemy.MetaData()
 
 
 @pytest.fixture(name="test_app")
 def test_app() -> FastAPI:
+    """
+    A fixture that re-initializes the FastAPI instance for test application.
+    """
     from app.main import initialize_application  # type: ignore
 
     return initialize_application(settings=settings)
 
 
-def create_and_drop_test_database():
+def create_and_drop_test_db() -> Generator[None, None, None]:
+    """
+    A function for creating database tables before testing and delete them at the end.
+    """
 
-    # Create test database and DB tables
+    TEST_DATABASE_URL = settings.database_url
+
+    # Databases query builder
+    # test_database = databases.Database(TEST_DATABASE_URL, force_rollback=True)
+
+    test_metadata = sqlalchemy.MetaData()
     test_engine = sqlalchemy.create_engine(
         TEST_DATABASE_URL, connect_args={"check_same_thread": False}, future=True, echo=True
     )
-    metadata.create_all(test_engine)
+    test_metadata.create_all(test_engine)
 
     # Run the test suite
     yield
 
     # Delete all DB tables
-    metadata.drop_all(test_engine)
+    test_metadata.drop_all(test_engine)
 
 
 @pytest.fixture(name="async_client")
 async def async_client(test_app: FastAPI) -> AsyncClient:
 
-    create_and_drop_test_database()
+    create_and_drop_test_db()
 
     # Initialize the test client for endpoint request
     with TestClient(test_app) as client:
