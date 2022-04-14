5b6a5b83efeedef9b3dce9cf9716b8c3be80a98a
Eternal-Engine <ninovationlab@gmail.com>
2022-04-15 00:58:34 +0200

    feat(test): Create a function to setup fake database pool in test application

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,10 +2,13 @@
 # type: ignore
 
 import pytest
+from asgi_lifespan import LifespanManager
+from asyncpg.pool import Pool
 from fastapi import FastAPI
 
 from app.core.config import get_settings
 from app.core.settings.app_base_settings import EnvTypes
+from tests.fake_asyncpg_pool import FakeAsyncPGPool
 
 # Set up the `app_env` to use the TEST environment settings
 settings = get_settings(app_env=EnvTypes.TEST)
@@ -20,3 +23,15 @@ def test_app() -> FastAPI:
     from app.main import initialize_application  # local import for testing purpose
 
     return initialize_application(settings=settings)
+
+
+@pytest.fixture(name="initialized_test_app")
+async def initialized_test_app(test_app: FastAPI) -> FastAPI:
+    async with LifespanManager(test_app):
+        test_app.state.pool = await FakeAsyncPGPool.create_pool(test_app.state.pool)
+        yield test_app
+
+
+@pytest.fixture(name="pool")
+def pool(initialized_test_app: FastAPI) -> Pool:
+    return initialized_test_app.state.pool
