47e3bdd52b9eab2fb1aff7f16681df8aacebeb0b
Eternal-Engine <ninovationlab@gmail.com>
2022-04-15 12:26:04 +0200

    feat: Create a function to convert datetime into string datatype

--- backend/app/models/base_models.py
+++ backend/app/models/base_models.py
@@ -1,5 +1,7 @@
 # type: ignore
 # fmot: off
-# pylint: disable=E1123
-def convert_datetime_into_string():
-    pass
+import datetime
+
+
+def convert_datetime_into_string(date_time: datetime.datetime) -> str:
+    return date_time.replace(tzinfo=datetime.timezone.utc).isoformat().replace("+00:00", "Z")
