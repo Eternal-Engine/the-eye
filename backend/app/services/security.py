17c7b4990105a3460b1499eb9b2585481234a53e
Eternal-Engine <ninovationlab@gmail.com>
2022-04-15 18:43:26 +0200

    feat(security): Create a function that generate the second layer hash with Argon2 algorithm for password security

--- backend/app/services/security.py
+++ backend/app/services/security.py
@@ -3,11 +3,13 @@ from typing import Any
 from passlib.context import CryptContext
 
 pwd_context_layer_1 = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context_layer_2 = CryptContext(schemes=["argon2"], deprecated="auto")
 
 
 def generate_layer_1_password_hash(layer_1: str) -> Any:
     return pwd_context_layer_1.hash(secret=layer_1)
 
 
-def get_password_hash():
-    pass
+def get_password_hash(layer_1: str, password: str) -> Any:
+
+    return pwd_context_layer_2.hash(secret=layer_1 + password)
