096320abf2df54c68acf2b2e52b35a76a2f9baa6
Eternal-Engine <ninovationlab@gmail.com>
2022-04-15 18:36:57 +0200

    feat(security): Create a function that generate layer 1 security for user's password

--- backend/app/services/security.py
+++ backend/app/services/security.py
@@ -1,2 +1,9 @@
-def generate_layer_1_pwd_security():
-    pass
+from typing import Any
+
+from passlib.context import CryptContext
+
+pwd_context_layer_1 = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def generate_layer_1_pwd_security(layer_1: str) -> Any:
+    return pwd_context_layer_1.hash(secret=layer_1)
