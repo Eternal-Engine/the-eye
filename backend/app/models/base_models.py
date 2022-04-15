a232b65954ac0444983062973269652fb0cf9366
Eternal-Engine <ninovationlab@gmail.com>
2022-04-15 12:28:31 +0200

    test(base-models): Ensure a function can convert a snake case field into camel case

--- backend/app/models/base_models.py
+++ backend/app/models/base_models.py
@@ -5,3 +5,7 @@ import datetime
 
 def convert_datetime_into_string(date_time: datetime.datetime) -> str:
     return date_time.replace(tzinfo=datetime.timezone.utc).isoformat().replace("+00:00", "Z")
+
+
+def convert_field_to_camel_case():
+    pass
--- backend/tests/unit_tests/test_models/test_base_models.py
+++ backend/tests/unit_tests/test_models/test_base_models.py
@@ -1,7 +1,7 @@
 # type: ignore
 import datetime
 
-from app.models.base_models import convert_datetime_into_string
+from app.models.base_models import convert_datetime_into_string, convert_field_to_camel_case
 
 
 def test_convert_datetime_into_str_datatype():
@@ -10,3 +10,11 @@ def test_convert_datetime_into_str_datatype():
 
     assert string_datetime[-1] == "Z"
     assert isinstance(string_datetime, str)
+
+
+def test_convert_field_into_camel_case():
+
+    field = "created_at"
+    camel_case_created_at_field = convert_field_to_camel_case(string=field)
+
+    assert camel_case_created_at_field == "CreatedAt"
