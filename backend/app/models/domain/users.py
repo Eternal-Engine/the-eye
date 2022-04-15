01b1ac7e1d99656c76e5cd1a9f3a42b09dff616b
Eternal-Engine <ninovationlab@gmail.com>
2022-04-15 19:30:57 +0200

    feat(domain models): Create User domain model that inherits from IWBaseModel

--- backend/app/models/domain/users.py
+++ backend/app/models/domain/users.py
@@ -1,3 +1,11 @@
 # type: ignore
-class User:
-    pass
+from app.models import base_models
+
+
+class User(base_models.IWBaseModel):
+    username: str
+    email: str
+    is_premium_account: bool = False
+    is_publisher: bool = False
+    is_verified: bool = False
+    is_active: bool = True
