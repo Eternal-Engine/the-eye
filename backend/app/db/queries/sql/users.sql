6a7898e8811e9d5ce6368dfc5ba63addca1cbd0e
Eternal-Engine <ninovationlab@gmail.com>
2022-04-17 16:35:40 +0200

    feat(sql): Create CRUD queries with SQL for UsersRepository
    - `create-user`
    - `read-users`
    - `read-user-by-id`
    - `read-user-by-username`
    - `read-user-by-email`
    - `update-user-by-id`
    - `update-user-by-username`
    - `delete-user-by-id`
    - `delete-user-by-username`

---
+++ backend/app/db/queries/sql/users.sql
@@ -0,0 +1,83 @@
+-- name: create-new-user<!
+INSERT INTO users (username, email, hashed_password)
+VALUES (:username, :email, :hashed_password)
+RETURNING
+    id, created_at, updated_at;
+
+
+-- name: read-users
+SELECT id,
+       username,
+       email
+FROM users;
+Limit 100;
+
+
+-- name: read-user-by-id^
+SELECT id,
+       username,
+       email,
+       hashed_password,
+       created_at,
+       updated_at
+FROM users
+WHERE id = :id
+LIMIT 1;
+
+
+-- name: read-user-by-username^
+SELECT id,
+       username,
+       email,
+       hashed_password,
+       created_at,
+       updated_at
+FROM users
+WHERE username = :username
+LIMIT 1;
+
+
+-- name: read-user-by-email^
+SELECT id,
+       username,
+       email,
+       hashed_password,
+       created_at,
+       updated_at
+FROM users
+WHERE email = :email
+LIMIT 1;
+
+
+-- name: update-user-by-id<!
+UPDATE
+    users
+SET username        = :new_username,
+    email           = :new_email,
+    hashed_password = :new_password,
+WHERE id = :id
+RETURNING
+    updated_at;
+
+
+-- name: update-user-by-username<!
+UPDATE
+    users
+SET username        = :new_username,
+    email           = :new_email,
+    hashed_password = :new_password,
+WHERE username = :username
+RETURNING
+    updated_at;
+
+
+--name: delete-user-by-id<!
+DELETE
+FROM users
+WHERE id = :id;
+
+
+--name: delete-user-by-username<!
+DELETE
+FROM users
+WHERE username = :username;
