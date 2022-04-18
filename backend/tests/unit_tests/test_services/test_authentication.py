ea3ad8ddeabc6dbf3e7767890d7d5c1d732e6b25
Eternal-Engine <ninovationlab@gmail.com>
2022-04-18 23:33:13 +0200

    feat: Update parameter to match the conventional name from repo into users_repo

--- backend/tests/unit_tests/test_services/test_authentication.py
+++ backend/tests/unit_tests/test_services/test_authentication.py
@@ -5,7 +5,7 @@ from app.services.authentication import authenticate_user, check_email_is_taken,
 async def test_email_is_taken(test_pool, test_user):
 
     async with test_pool.acquire() as conn:
-        is_email_taken = await check_email_is_taken(repo=users_repo.UsersRepository(conn), email=test_user.email)
+        is_email_taken = await check_email_is_taken(users_repo=users_repo.UsersRepository(conn), email=test_user.email)
 
     assert is_email_taken is True
 
@@ -14,7 +14,7 @@ async def test_email_is_not_taken(test_pool):
 
     async with test_pool.acquire() as conn:
         is_email_taken = await check_email_is_taken(
-            repo=users_repo.UsersRepository(conn), email="email.still.available@test.com"
+            users_repo=users_repo.UsersRepository(conn), email="email.still.available@test.com"
         )
 
     assert is_email_taken is False
@@ -24,7 +24,7 @@ async def test_username_is_taken(test_pool, test_user):
 
     async with test_pool.acquire() as conn:
         is_username_taken = await check_username_is_taken(
-            repo=users_repo.UsersRepository(conn), username=test_user.username
+            users_repo=users_repo.UsersRepository(conn), username=test_user.username
         )
 
     assert is_username_taken is True
@@ -34,7 +34,7 @@ async def test_username_is_not_taken(test_pool):
 
     async with test_pool.acquire() as conn:
         is_email_taken = await check_username_is_taken(
-            repo=users_repo.UsersRepository(conn), username="availableusername"
+            users_repo=users_repo.UsersRepository(conn), username="availableusername"
         )
 
     assert is_email_taken is False
@@ -44,7 +44,7 @@ async def test_user_authentication_failed_with_no_user_found(test_pool):
 
     async with test_pool.acquire() as conn:
         is_user_authenticated = await authenticate_user(
-            repo=users_repo.UsersRepository(conn), username="invalidusername", password="fake-password"
+            users_repo=users_repo.UsersRepository(conn), email="invalid.email@invaliden.com", password="fake-password"
         )
 
     assert is_user_authenticated is False
@@ -54,7 +54,7 @@ async def test_user_authentication_failed_with_unmatched_password(test_pool, tes
 
     async with test_pool.acquire() as conn:
         is_user_authenticated = await authenticate_user(
-            repo=users_repo.UsersRepository(conn), username=test_user.username, password="fake-password"
+            users_repo=users_repo.UsersRepository(conn), email=test_user.email, password="fake-password"
         )
 
     assert is_user_authenticated is False
@@ -64,7 +64,7 @@ async def test_user_successfully_authenticated(test_pool, test_user):
 
     async with test_pool.acquire() as conn:
         is_user_authenticated = await authenticate_user(
-            repo=users_repo.UsersRepository(conn), username=test_user.username, password="password-test"
+            users_repo=users_repo.UsersRepository(conn), email=test_user.email, password="password-test"
         )
 
     assert is_user_authenticated.dict(exclude={"created_at", "updated_at"}) == test_user.dict(
