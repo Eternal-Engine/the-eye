58da04ed9ef1ae04a840e5cd0c89228cfbf095b6
Eternal-Engine <ninovationlab@gmail.com>
2022-04-18 18:01:32 +0200

    test(services): Ensure that jwt token can be generated and saved into JWToken and JWTUser classes

---
+++ backend/tests/unit_tests/test_services/test_jwt.py
@@ -0,0 +1,32 @@
+# type: ignore
+import datetime
+
+from jose import jwt as jose_jwt
+
+from app.services import jwt as jwt_services
+from app.services.config import SecuritySettings
+
+
+async def test_generate_jwt_token(test_user):
+
+    token = jwt_services.generate_jwt_token(
+        jwt_data={"username": test_user.username},
+        secret_key="fake-secret",
+        expires_delta=datetime.timedelta(minutes=1),
+    )
+
+    assert isinstance(token, str)
+
+    parsed_payload = jose_jwt.decode(token, "fake-secret", algorithms=[SecuritySettings.ALGORITHM_JWT])
+
+    assert parsed_payload["username"] == "usertest"
+    assert parsed_payload["sub"] == "access"
+
+
+async def test_generate_access_token(test_user):
+
+    token = jwt_services.generate_access_token(user=test_user, secret_key="fake-secret")
+    parsed_payload = jose_jwt.decode(token, "fake-secret", algorithms=[SecuritySettings.ALGORITHM_JWT])
+
+    assert parsed_payload["username"] == "usertest"
+    assert parsed_payload["sub"] == "access"
