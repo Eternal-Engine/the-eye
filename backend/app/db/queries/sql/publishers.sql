83a4175b3a7290b9b9c70391c95528a7c2295846
Eternal-Engine <ninovationlab@gmail.com>
2022-04-23 13:47:47 +0200

    feat(publishers): Create SQL queries for publishers table and indent update query for journalists

--- backend/app/db/queries/sql/publishers.sql
+++ backend/app/db/queries/sql/publishers.sql
@@ -1,14 +1,13 @@
--- name: create-new-journalist<!
-INSERT INTO journalists (first_name, last_name, profile_picture, banner, bio, address, postal_code, state, country, office_phone_number, mobile_phone_number, user_id)
-VALUES (:first_name, :last_name, :profile_picture, :banner, :bio, :address, :postal_code, :state, :country, :office_phone_number, :mobile_phone_number, :user_id)
+-- name: create-new-publisher<!
+INSERT INTO publishers (name, profile_picture, banner, bio, address, postal_code, state, country, office_phone_number, mobile_phone_number, user_id)
+VALUES (:name, :profile_picture, :banner, :bio, :address, :postal_code, :state, :country, :office_phone_number, :mobile_phone_number, :user_id)
 RETURNING
     id, created_at, updated_at;
 
 
--- name: read-journalist-by-user-id^
+-- name: read-publisher-by-user-id^
 SELECT  id,
-        first_name,
-        last_name,
+        name,
         profile_picture,
         banner,
         bio,
@@ -21,16 +20,15 @@ SELECT  id,
         user_id,
         created_at,
         updated_at
-FROM journalists
+FROM publishers
 WHERE user_id = :user_id
 LIMIT 1;
 
 
--- name: update-journalist-by-id<!
+-- name: update-publisher-by-id<!
 UPDATE
     journalists
-SET first_name          = :new_first_name,
-    last_name           = :new_last_name,
+SET name                = :new_name,
     profile_picture     = :new_profile_picture,
     banner              = :new_banner,
     bio                 = :new_bio,
