bce49a811b3faa6ba202f2c9c6fc0e11ffb264ca
Eternal-Engine <ninovationlab@gmail.com>
2022-04-23 11:22:42 +0200

    ref(http exceptions): Group http exceptions details messages into 1 function for each HTTP exception error

--- backend/app/api/exceptions/http_exc_403.py
+++ backend/app/api/exceptions/http_exc_403.py
@@ -1,8 +1,10 @@
 import fastapi
 
+from app.resources.http_exc_details import HTTP_300_DETAILS
+
 
 async def http403_exc_forbidden() -> Exception:
     raise fastapi.HTTPException(
         status_code=fastapi.status.HTTP_403_FORBIDDEN,
-        detail="Unable to validate credentials; Check the JWT token or login credentials!",
+        detail=HTTP_300_DETAILS,
     )
--- backend/app/api/exceptions/http_exc_404.py
+++ backend/app/api/exceptions/http_exc_404.py
@@ -1,15 +1,17 @@
 import fastapi
 
+from app.resources.http_exc_details import http_404_details
+
 
 async def http404_exc_id_not_found(id: int) -> Exception:
     raise fastapi.HTTPException(
         status_code=fastapi.status.HTTP_404_NOT_FOUND,
-        detail=f"Either the user with ID {id} is deleted, or you are not authorized; Check your authorization!",
+        detail=http_404_details(id=id),
     )
 
 
 async def http404_exc_username_not_found(username: str) -> Exception:
     raise fastapi.HTTPException(
         status_code=fastapi.status.HTTP_404_NOT_FOUND,
-        detail=f"Either the user {username} is deleted, or you are not authorized; Check your authorization!",
+        detail=http_404_details(username=username),
     )
