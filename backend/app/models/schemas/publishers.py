7fdef515b7230225918081f0ce3f58678d596a72
Eternal-Engine <ninovationlab@gmail.com>
2022-04-23 23:44:20 +0200

    feat(publishers): Create the schema model for Publisher

--- backend/app/models/schemas/publishers.py
+++ backend/app/models/schemas/publishers.py
@@ -6,8 +6,17 @@ from app.models.domain.publishers import Publisher
 from app.models.schemas.base import IWBaseSchema  # type: ignore
 
 
-class PublisherInResponse(IWBaseSchema):  # type: ignore
-    profile: Publisher
+class PublisherInCreate(IWBaseSchema):
+    name: Optional[str] = None
+    profile_picture: Optional[str] = None
+    banner: Optional[str] = None
+    bio: Optional[str] = None
+    address: Optional[str] = None
+    postal_code: Optional[str] = None
+    state: Optional[str] = None
+    country: Optional[str] = None
+    office_phone_number: Optional[str] = None
+    mobile_phone_number: Optional[str] = None
 
 
 class PublisherInUpdate(pydantic.BaseModel):
@@ -16,3 +25,13 @@ class PublisherInUpdate(pydantic.BaseModel):
     profile_picture: Optional[str] = None
     banner: Optional[str] = None
     bio: Optional[str] = None
+    address: Optional[str] = None
+    postal_code: Optional[str] = None
+    state: Optional[str] = None
+    country: Optional[str] = None
+    office_phone_number: Optional[str] = None
+    mobile_phone_number: Optional[str] = None
+
+
+class PublisherInResponse(IWBaseSchema):  # type: ignore
+    publisher: Publisher
