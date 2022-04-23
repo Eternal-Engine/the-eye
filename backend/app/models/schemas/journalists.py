8116380fdd29b461657a6debb594cb560f74b9a2
Eternal-Engine <ninovationlab@gmail.com>
2022-04-23 23:29:46 +0200

    feat(journalists): Create schema model JournalistInCreate for creating a journalist

--- backend/app/models/schemas/journalists.py
+++ backend/app/models/schemas/journalists.py
@@ -6,8 +6,18 @@ from app.models.domain.journalists import Journalist
 from app.models.schemas.base import IWBaseSchema  # type: ignore
 
 
-class JournalistInResponse(IWBaseSchema):  # type: ignore
-    profile: Journalist
+class JournalistInCreate(IWBaseSchema):
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    profile_picture: Optional[str] = None
+    banner: Optional[str] = None
+    bio: Optional[str] = None
+    address: Optional[str] = None
+    postal_code: Optional[str] = None
+    state: Optional[str] = None
+    country: Optional[str] = None
+    office_phone_number: Optional[str] = None
+    mobile_phone_number: Optional[str] = None
 
 
 class JournalistInUpdate(pydantic.BaseModel):
@@ -23,3 +33,7 @@ class JournalistInUpdate(pydantic.BaseModel):
     country: Optional[str] = None
     office_phone_number: Optional[str] = None
     mobile_phone_number: Optional[str] = None
+
+
+class JournalistInResponse(IWBaseSchema):  # type: ignore
+    journalist: Journalist
