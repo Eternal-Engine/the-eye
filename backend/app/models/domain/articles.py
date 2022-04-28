3ac06da2906163c636b27c3a12c72106d98c086e
chindrop <apronticaleb@gmail.com>
2022-04-28 19:21:57 +0000

    Created Domain.article had Article and Article in DB

---
+++ backend/app/models/domain/articles.py
@@ -0,0 +1,30 @@
+
+from app.models.domain.base import IWBaseModel
+from app.models.mixins.identifier import IDModelMixin
+from app.models.mixins.date_time import DateTimeModelMixin
+from app.models.domain.journalists import Journalist
+import slugify
+
+
+
+class Article(IWBaseModel):
+   
+    headline: str
+    description: str
+    body: str
+    #images: List[Image]
+    #videos: List[Video]
+    #audios: List[Audio]
+    #tags: List[Tag]
+    #comments: List[Comment]
+    #likes: List[Like]
+    author: Journalist
+    author_id: int
+    is_drafted: bool = False
+
+
+class ArticleInDB(Article, IDModelMixin, DateTimeModelMixin):
+    slug = ""
+
+    def generate_slug(self, headline, journalist_id, article_id):
+        self.slug = slugify.slugify(f"{headline}-{journalist_id}{article_id}")
